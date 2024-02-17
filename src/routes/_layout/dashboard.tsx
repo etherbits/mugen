4afbdd1514b866b4b89942847b37d4b9a40f0e8a
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-02-17 21:11:28 +0400

    returning dashboard component from dashboard route

--- src/routes/_layout/dashboard.tsx
+++ src/routes/_layout/dashboard.tsx
@@ -1,10 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_layout/dashboard')({
-  component: () => <div>Hello /_layout/dashboard!</div>
+  component: Dashboard,
 })
 
-export default function Dashboard() {
+function Dashboard() {
   return (
     <div>Dashboard</div>
   )
