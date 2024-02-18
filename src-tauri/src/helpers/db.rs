c6de05b656c4e8168eb04ca419f0070bdd158f5d
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-02-18 20:14:41 +0400

    cleaning up main a bit

---
+++ src-tauri/src/helpers/db.rs
@@ -0,0 +1,54 @@
+const SHOULD_RESET_DB: bool = true;
+
+use std::{path::PathBuf, sync::Mutex};
+
+use rusqlite::Connection;
+
+use crate::helpers::fs::{does_file_exist, get_string_from_path_buf};
+
+pub struct DB {
+    pub connection: Mutex<Connection>,
+}
+
+pub fn create_db_tables(connection: &Connection) {
+    match connection.execute("CREATE TABLE habits (name TEXT);", ()) {
+        Ok(_) => println!("Habits table created"),
+        Err(_) => println!("Error while creating habits table"),
+    };
+}
+
+pub fn delete_db_tables(connection: &Connection) {
+    match connection.execute("DROP TABLE habits;", ()) {
+        Ok(_) => println!("Habits table deleted"),
+        Err(_) => println!("Error while deleting habits table"),
+    };
+}
+
+pub fn init_db(base_data_path: PathBuf) -> DB {
+    let file_path_buf = base_data_path.join("habits.db");
+    let file_path = get_string_from_path_buf(file_path_buf);
+    let file_exists = does_file_exist(&file_path);
+
+    println!("Database file path: {}", file_path);
+
+    let connection = match Connection::open(&file_path) {
+        Ok(connection) => connection,
+        Err(_) => {
+            println!("Error while opening database");
+            std::process::exit(1);
+        }
+    };
+
+    if !file_exists {
+        create_db_tables(&connection);
+    }
+
+    if SHOULD_RESET_DB {
+        delete_db_tables(&connection);
+        create_db_tables(&connection);
+    }
+
+    DB {
+        connection: Mutex::new(connection),
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-const SHOULD_RESET_DB: bool = true;
-
 mod helpers;
-
-use helpers::fs::{does_file_exist, get_string_from_path_buf};
-
-use rusqlite::Connection;
-use std::{path::PathBuf, sync::Mutex};
+use helpers::db::{init_db, DB};
 use tauri::{Manager, State};
-
-struct DB {
-    connection: Mutex<Connection>,
-}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -31,50 +21,7 @@ fn add_habit(habit_name: String, db_state: State<DB>) -> String {
         }
     }
 
-    format!("Hello, {}! You've been greeted from Rust!", habit_name)
-}
-
-fn create_db_tables(connection: &Connection) {
-    match connection.execute("CREATE TABLE habits (name TEXT);", ()) {
-        Ok(_) => println!("Habits table created"),
-        Err(_) => println!("Error while creating habits table"),
-    };
-}
-
-fn delete_db_tables(connection: &Connection) {
-    match connection.execute("DROP TABLE habits;", ()) {
-        Ok(_) => println!("Habits table deleted"),
-        Err(_) => println!("Error while deleting habits table"),
-    };
-}
-
-fn init_db(base_data_path: PathBuf) -> DB {
-    let file_path_buf = base_data_path.join("habits.db");
-    let file_path = get_string_from_path_buf(file_path_buf);
-    let file_exists = does_file_exist(&file_path);
-
-    println!("Database file path: {}", file_path);
-
-    let connection = match Connection::open(&file_path) {
-        Ok(connection) => connection,
-        Err(_) => {
-            println!("Error while opening database");
-            std::process::exit(1);
-        }
-    };
-
-    if !file_exists {
-        create_db_tables(&connection);
-    }
-
-    if SHOULD_RESET_DB {
-        delete_db_tables(&connection);
-        create_db_tables(&connection);
-    }
-
-    DB {
-        connection: Mutex::new(connection),
-    }
+    format!("{} was added to the db", habit_name)
 }
 
 fn main() {
